39bff9c6a792118475e7c0e73859ec9d1540f39a
Pat George <pat.george@hungrymachine.com>
2015-07-27 21:09:23 -0400

    Add CardType

---
+++ app/models/card_type.rb
@@ -0,0 +1,3 @@
+class CardType < ActiveRecord::Base
+  validates_uniqueness_of :name
+end
---
+++ db/migrate/20150727234920_create_card_types.rb
@@ -0,0 +1,9 @@
+class CreateCardTypes < ActiveRecord::Migration
+  def change
+    create_table :card_types do |t|
+      t.string :name, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
