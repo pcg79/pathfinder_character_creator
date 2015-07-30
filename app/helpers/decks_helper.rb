4e40ccb3fe75d4f348a9f9767d2b879b8329ef56
Pat George <pat.george@hungrymachine.com>
2015-07-30 09:40:57 -0400

    Refactor how statuses are created.
    And set statuses whenever cards are added or deleted.

--- app/helpers/decks_helper.rb
+++ app/helpers/decks_helper.rb
@@ -1,5 +1,7 @@
 module DecksHelper
   def card_type_status_class(deck, type)
+    type = status_type(type)
+
     current_count = deck.cards.send(type.to_sym).count
 
     limit = deck.send("#{type}_count".to_sym)
@@ -11,5 +13,13 @@ module DecksHelper
     else
       "under"
     end
+
+  rescue NoMethodError => e
+    Rails.logger.error "*** [card_type_status_class] NoMethodError caught.  #{e.message}"
+    ''
+  end
+
+  def status_type(type)
+    type.downcase.pluralize
   end
 end
