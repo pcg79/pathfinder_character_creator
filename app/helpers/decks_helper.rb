9f4afcb4c197c63b0d6a34fc5384fcc714277c97
Pat George <pat.george@hungrymachine.com>
2015-07-30 09:17:35 -0400

    Added helper for card type count status

--- app/helpers/decks_helper.rb
+++ app/helpers/decks_helper.rb
@@ -1,2 +1,15 @@
 module DecksHelper
+  def card_type_status_class(deck, type)
+    current_count = deck.cards.send(type.to_sym).count
+
+    limit = deck.send("#{type}_count".to_sym)
+
+    if current_count == limit
+      "valid"
+    elsif current_count > limit
+      "over"
+    else
+      "under"
+    end
+  end
 end
